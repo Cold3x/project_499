d94b35159ca2117d23be37d16f17f9b9a60c597c
Cold3x <rqkung@gmail.com>
2023-04-05 16:30:51 +0700

    init

--- src/index.js
+++ src/index.js
@@ -22,15 +22,18 @@ const port = 8082
 const bundler = new Bundler(bundlePath);
 
 app.use(bundler.middleware());
-// app.use(router)
 
-const server = http.createServer(app);
-server.listen(port);
+app.listen(port, ()=>{
+    console.log("Server Start at 8082");
+})
 
-server.on('error', (err) => console.error(err));
-server.on('listening', () => {
-    console.info('Server is running');
-    console.info(`  NODE_ENV=[${process.env.NODE_ENV}]`);
-    console.info(`  Port=[${port}]`);
-    // open(`http://localhost:${port}`);
-});
+// const server = http.createServer(app);
+// server.listen(port);
+
+// server.on('error', (err) => console.error(err));
+// server.on('listening', () => {
+//     console.info('Server is running');
+//     console.info(`  NODE_ENV=[${process.env.NODE_ENV}]`);
+//     console.info(`  Port=[${port}]`);
+//     // open(`http://localhost:${port}`);
+// });
