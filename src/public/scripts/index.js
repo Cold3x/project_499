e6074ce9e7ca51cc59cc3ad07e17e70c5e42d233
Cold3x <rqkung@gmail.com>
2023-04-06 19:33:53 +0700

    fix

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -50,8 +50,8 @@ function createHM(gazeInfo) {
 function onClickCalibrationBtn() {
   const userId = "YOUR_USER_ID";
   // Next Page after calibration
-  // const redirectUrl = "https://project-eyetrack.onrender.com";
-  const redirectUrl = "http://localhost:8082/";
+  const redirectUrl = "https://project-eyetrack.onrender.com";
+  // const redirectUrl = "http://localhost:8082/";
   const calibrationPoint = 5;
   EasySeeSo.openCalibrationPage(
     licenseKey,
